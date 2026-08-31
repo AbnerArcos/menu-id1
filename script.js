ba6b42e676a2d4741b800abfdcd8bdd4b6e6c769
GlackHits <07.alexacosta@gmail.com>
2026-08-30 22:22:38 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-const paymentCard = document.getElementById("paymentCard");
-function mostrarPago(){
-  paymentCard.classList.add("active");
-}
-function cerrarPago(){
-  paymentCard.classList.remove("active");
-}
